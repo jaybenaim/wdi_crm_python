8e59c9dec8c8f3e4cd8844c92323877f9464108f
jaybenaim <benaimjacob@gmail.com>
2019-07-23 22:18:46 -0400

    reformatted using black

--- crm.py
+++ crm.py
@@ -2,6 +2,7 @@ from contact import Contact
 import sys
 import sqlite3
 
+
 class CRM:
     def main_menu(self):
         try:
@@ -34,7 +35,7 @@ class CRM:
             self.search_by_id()
         elif user_selected == 6:
             sys.exit("Goodbye!")
-            close() 
+            close()
         else:
             return "You entered an invalid selection"
 
@@ -44,96 +45,94 @@ class CRM:
         last_name = input("Enter Last Name:\n")
         email = input("Enter Email Address:\n")
         note = input("Enter a Note:\n")
-        
-        contact = Contact.create(
-            first_name=first_name,
-            last_name=last_name,
-            email=email,
-            note=note)
 
-        
+        contact = Contact.create(
+            first_name=first_name, last_name=last_name, email=email, note=note
+        )
 
     def modify_existing_contact(self):
-        
+
         user_id = input("Please enter your id number\n")
-        attribute_to_update = input("What are you trying to change? \n (first_name, last_name, email, note)\n").lower()
+        attribute_to_update = input(
+            "What are you trying to change? \n (first_name, last_name, email, note)\n"
+        ).lower()
         value = input("Which would you like to change it to?\n")
-        
-        try: 
-            cur=Contact.db.cursor() 
-            if attribute_to_update == 'first_name': 
-                qry=f"update contact set first_name=? where id=?"
-                cur.execute(qry,(f'{value}', f'{user_id}'))
+
+        try:
+            cur = Contact.db.cursor()
+            if attribute_to_update == "first_name":
+                qry = f"update contact set first_name=? where id=?"
+                cur.execute(qry, (f"{value}", f"{user_id}"))
                 Contact.db.commit()
-                print('record updated succesfully')
-            if attribute_to_update == 'last_name': 
-                qry=f"update contact set last_name=? where id=?"
-                cur.execute(qry,(f'{value}', f'{user_id}'))
-                print('record updated succesfully')
-            if attribute_to_update == 'email': 
-                qry=f"update contact set email=? where id=?"
-                cur.execute(qry,(f'{value}', f'{user_id}'))
-                print('record updated succesfully')
-            if attribute_to_update == 'note': 
-                qry=f"update contact set note=? where id=?"
-                cur.execute(qry,(f'{value}', f'{user_id}'))
-                print('record updated succesfully')
-        except: 
+                print("record updated succesfully")
+            if attribute_to_update == "last_name":
+                qry = f"update contact set last_name=? where id=?"
+                cur.execute(qry, (f"{value}", f"{user_id}"))
+                print("record updated succesfully")
+            if attribute_to_update == "email":
+                qry = f"update contact set email=? where id=?"
+                cur.execute(qry, (f"{value}", f"{user_id}"))
+                print("record updated succesfully")
+            if attribute_to_update == "note":
+                qry = f"update contact set note=? where id=?"
+                cur.execute(qry, (f"{value}", f"{user_id}"))
+                print("record updated succesfully")
+        except:
             print("something went wrong")
-            # Contact.rollback() 
-            # Contact.db.close() 
-        
+            # Contact.rollback()
+            # Contact.db.close()
+
         # user_id = input("Please enter your id number\n")
         # attribute_to_update = input("What are you trying to change? \n (first name, last name, email, note)\n").lower()
         # value = input("Which would you like to change it to?\n")
 
         # contact = Contact.get(id=user_id)
         # if user_id == contact.id:
-        #     if attribute_to_update == 'first name': 
+        #     if attribute_to_update == 'first name':
         #         contact.first_name = value
-        #         contact.save()  
-        #     elif attribute_to_update == 'last name': 
+        #         contact.save()
+        #     elif attribute_to_update == 'last name':
         #         contact.last_name = value
-        #         contact.save()  
-        #     elif attribute_to_update == 'email': 
+        #         contact.save()
+        #     elif attribute_to_update == 'email':
         #         contact.email = value
         #         contact.save()
-        #     elif attribute_to_update == 'note': 
+        #     elif attribute_to_update == 'note':
         #         contact.note = value
-        #         contact.save() 
+        #         contact.save()
 
-        
     def delete_contact(self):
-        contact_to_delete = input("Which contact would you like to delete? \nEnter the id for the contact you wish to delete:\t")
+        contact_to_delete = input(
+            "Which contact would you like to delete? \nEnter the id for the contact you wish to delete:\t"
+        )
         contact = Contact.get(id=contact_to_delete)
-        confirm = input('Are you sure you would like to delete this contact?\t (y/n)').lower()
-        
-        if confirm == 'y': 
-            contact.delete_instance() 
+        confirm = input(
+            "Are you sure you would like to delete this contact?\t (y/n)"
+        ).lower()
+        if confirm == "y":
+            contact.delete_instance()
             print("Succesfully deleted contact!")
-        else: 
-            self.main_menu() 
-        
-    def display_all_contacts(self):
+        else:
+            self.main_menu()
 
+    def display_all_contacts(self):
         cur = Contact.db.cursor()
         cur.execute("SELECT * FROM contact;")
-        all_contacts = cur.fetchall() 
-        
-        for contact in all_contacts: 
-            print(contact) 
-        
+        all_contacts = cur.fetchall()
+        for contact in all_contacts:
+            print(contact)
 
     def search_by_id(self):
         contact_id = input("What is the id of the contact you are searching for? \n")
         contact = Contact.get(id=contact_id)
-        print(f'First Name: {contact.first_name} Last Name: {contact.last_name} Email: {contact.email} Note: {contact.note}')
+        print(
+            f"First Name: {contact.first_name} Last Name: {contact.last_name} Email: {contact.email} Note: {contact.note}"
+        )
         return contact.id
         # print(Contact.find_by(search_by_id, search_by_value)
-        # #which_search_method = ("Would you like to search by the 'ids' or 'values'? \n").lower() 
+        # #which_search_method = ("Would you like to search by the 'ids' or 'values'? \n").lower()
         # search_by_id = input("What id would you like to search for?\n ")
         # search_by_value = input("Which value would you like to search for?\n")
-
 
 
 crm = CRM()
